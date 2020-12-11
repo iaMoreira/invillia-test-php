682291527fb93220a9add49911e1415a4fd0ffed
Ian Moreira <ianmoreira80@gmail.com>
2020-12-11 18:02:30 -0300

    feature: add base de telefones

--- app/Models/Phone.php
+++ app/Models/Phone.php
@@ -4,7 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Person extends Model
+class Phone extends Model
 {
-    protected $fillable = ['name'];
+    protected $fillable = ['user_id', 'phone'];
+
 }

92d78123e5fee0d4b35b021e8487b0f16f4df923
Ian Moreira <ianmoreira80@gmail.com>
2020-12-13 14:26:17 -0300

    refactor: UploadController

--- app/Http/Controllers/UploadController.php
+++ app/Http/Controllers/UploadController.php
@@ -12,32 +12,31 @@ use Illuminate\Support\Facades\DB;
 
 class UploadController extends Controller
 {
-    public function people(Request $request) {
+    public function people(Request $request)
+    {
         $people = simplexml_load_file($request->file);
-        if(!isset($people->person)) {
+        if (!isset($people->person)) {
             throw new \Exception('Error parser');
         }
-        
+
         DB::beginTransaction();
         try {
-            foreach($people as $personXml) {
-                $person = new Person();
-                $person->id = intval($personXml->personid);
-                $person->name = strval($personXml->personname);
-                $phones = array();
-                $person->save();
-                foreach($personXml->phones->phone as $phoneXml) {
-                    $phone = new Phone();
-                    $phone->phone = strval($phoneXml);
-                    $phone->person_id = $person->id;
-                    $phone->save();
-                    $phones[] = $phone;
+            foreach ($people as $personXml) {
+                $person = Person::updateOrCreate(
+                    array('name' => strval($personXml->personname)),
+                    array('id' => intval($personXml->personid))
+                );
+                foreach ($personXml->phones->phone as $phoneXml) {
+                    Phone::updateOrCreate(
+                        array('phone' => strval($phoneXml)),
+                        array('person_id' => $person->id)
+                    );
                 }
             }
 
             // DB::rollback();
             DB::commit();
-        } catch(\Exception $e) {
+        } catch (\Exception $e) {
             DB::rollback();
             dd($e);
         }
@@ -45,41 +44,46 @@ class UploadController extends Controller
         return redirect()->route('home');
     }
 
-    public function orders(Request $request) {
+    public function orders(Request $request)
+    {
         $shiporders = simplexml_load_file($request->file);
-        if(!isset($shiporders->shiporder)) {
+        if (!isset($shiporders->shiporder)) {
             throw new \Exception('Error parser');
         }
-        
+
         DB::beginTransaction();
         try {
-            foreach($shiporders as $shiporder) {
-                $order = new Order();
-                $order->id = intval($shiporder->orderid);
-                $order->person_id = intval($shiporder->orderperson);
-                $order->save();
-                $address = new Address();
-                $address->name = strval($shiporder->shipto->name);
-                $address->address = strval($shiporder->shipto->address);
-                $address->city = strval($shiporder->shipto->city);
-                $address->country = strval($shiporder->shipto->country);
-                $address->order_id = $order->id;
-                $address->save();
+            foreach ($shiporders as $shiporder) {
+                $order = Order::updateOrCreate(
+                    array('person_id' => intval($shiporder->orderperson)),
+                    array('id' => intval($shiporder->orderid))
+                );
 
-                foreach($shiporder->items->item as $itemXml) {
-                    $item = new Item();
-                    $item->title = strval($itemXml->title);
-                    $item->note = strval($itemXml->note);
-                    $item->quantity = intval($itemXml->quantity);
-                    $item->price = floatval($itemXml->price);
-                    $item->order_id = $order->id;
-                    $item->save();
+                Address::updateOrCreate(
+                    array(
+                        'name' => strval($shiporder->shipto->name),
+                        'address' => strval($shiporder->shipto->address),
+                        'city' => strval($shiporder->shipto->city),
+                        'country' => strval($shiporder->shipto->country),
+                    ),
+                    array('order_id' => $order->id)
+
+                );
+
+                foreach ($shiporder->items->item as $itemXml) {
+                    Item::updateOrCreate(
+                        array(
+                            'title' => strval($itemXml->title),
+                            'note' => strval($itemXml->note),
+                            'quantity' => intval($itemXml->quantity),
+                            'price' => floatval($itemXml->price),
+                        ),
+                        array('order_id' => $order->id)
+                    );
                 }
             }
-
-            // DB::rollback();
             DB::commit();
-        } catch(\Exception $e) {
+        } catch (\Exception $e) {
             DB::rollback();
             dd($e);
         }
